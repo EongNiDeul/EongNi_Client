4a2ca9f12a73ad96458727afd93a41df9bf8ad9d
hofkj <carrot070410@naver.com>
2025-05-30 00:46:20 +0900

    Feat : 로그인 페이지 제작

--- eongni_client/src/pages/LoginPage.jsx
+++ eongni_client/src/pages/LoginPage.jsx
@@ -3,32 +3,33 @@ import styles from "../styles/pages/LoginPage.module.css";
 
 function LoginPage() {
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.title}>로그인</div>
+    <div className={styles.container}>
+      <div className={styles.title}>로그인</div>
 
-        <div>
-          <label>
-            {" "}
-            닉네임
-            <input type="text" placeholder="닉네임을 입력해주세요" className={styles.input}/>
-          </label>
+      <form className={styles.form}>
+        <div className={styles.inputGroup}>
+          <label htmlFor="nickname">닉네임</label>
+          <input
+            id="nickname"
+            type="text"
+            placeholder="닉네임을 입력해주세요"
+            className={styles.input}
+          />
         </div>
 
-        <div>
-          <label>
-            {" "}
-            비밀번호
-            <input
-              type="password"
-              placeholder="비밀번호를 입력해 주세요 ( 8자리이하 : 영어, 숫자, 특수문자 포함 ) 입력해주세요"
-              className={styles.input}
-            />
-          </label>
+        <div className={styles.inputGroup}>
+          <label htmlFor="password">비밀번호</label>
+          <input
+            id="password"
+            type="password"
+            placeholder="비밀번호를 입력해 주세요 (영어, 숫자, 특수문자 포함)"
+            className={styles.input}
+          />
         </div>
-        <button>로그인</button>
-      </div>
-    </>
+
+        <button className={styles.loginBtn}>로그인</button>
+      </form>
+    </div>
   );
 }
 
