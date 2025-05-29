a8d61ee59a5c233948f7bb280f898af80eb695f2
hofkj <carrot070410@naver.com>
2025-05-29 10:25:08 +0900

    Feat : test.jsx

---
+++ eongni_client/src/components/test.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function test() {
+  return <>엉니가 했어</>;
+}
+
+export default test;
--- eongni_client/src/pages/LoginPage.jsx
+++ eongni_client/src/pages/LoginPage.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import styles from "../styles/pages/LoginPage.module.css";
 
 function LoginPage() {
-
   return (
     <>
-     <div>로그인</div>
+      <div className={styles.container}>
+        <div className={styles.title}>로그인</div>
+
+        <div>
+          <label>
+            {" "}
+            닉네임
+            <input type="text" placeholder="닉네임을 입력해주세요" className={styles.input}/>
+          </label>
+        </div>
+
+        <div>
+          <label>
+            {" "}
+            비밀번호
+            <input
+              type="password"
+              placeholder="비밀번호를 입력해 주세요 ( 8자리이하 : 영어, 숫자, 특수문자 포함 ) 입력해주세요"
+              className={styles.input}
+            />
+          </label>
+        </div>
+        <button>로그인</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
